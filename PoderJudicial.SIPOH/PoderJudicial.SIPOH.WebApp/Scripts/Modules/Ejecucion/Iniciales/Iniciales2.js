b2b3efac7d883518f1bcbd001ed4b19f8207039b
PBTO90 <bcss.90@outlook.com>
2020-08-27 13:27:09 -0500

    Se agrega metodo generico para data tables

--- PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Scripts/Modules/Ejecucion/Iniciales/Iniciales2.js
+++ PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Scripts/Modules/Ejecucion/Iniciales/Iniciales2.js
@@ -1,3 +1,106 @@
-﻿$(document).ready(function () {
- 
+﻿var estructuraTablaCausas =
+    [{ data: 'nJuzgado', title: 'N° Juzgado' },
+     { data: 'causaNuc', title: 'Causa|Nuc' },
+     { data: 'ofendido', title: 'Ofendido (s)' },
+     { data: 'inculpado', title: 'Inculpado (s)' },
+     { data: 'delito', title: 'Delitos (s)' },
+     { data: 'eliminar', title: 'Quitar' }];
+
+var causas = [];
+var dtabla = null;
+
+$(document).ready(function ()
+{
+    var nombreTabla = "dataTableCausas";
+
+    for (var index = 0; index < 500; index++)
+    {
+        var causa = new Object();
+        causa.id = index;
+        causa.nJuzgado = "Juzgado de Ejecucion Pro Pachuca";
+        causa.causaNuc = "0027/2017";
+        causa.ofendido = "ANTOLIN ALFREDO ORTIZ GUTIERREZ, JOSE SOCORRO HERNANDEZ VELAZQUEZ";
+        causa.inculpado = "FERNANDO FUENTES CRUZ, SERGIO ALBERTO VALENTIN MONZALVO";
+        causa.delito = "ASALTO AGRAVADO, ROBO";
+        causa.eliminar = '<a href= "#" onclick="imprimirAlert(' + index + ')"><div class="btn btn-danger btn-sm"><i class="fa fa-close"></i></div></a>'
+        causas.push(causa);
+    }
+
+    dtabla = GeneraTablaDatos(dtabla, nombreTabla, causas, estructuraTablaCausas);
 });
+
+function imprimirAlert(index)
+{
+    alert(index);
+}
+
+function GeneraTablaDatos(tabla, idTablaHtml, datos, estructuraTabla, mostrar)
+{
+    if (tabla != null)
+    {
+        tabla.destroy();
+        $("#" + idTablaHtml).empty();
+    }
+
+    return tabla = $("#" + idTablaHtml).DataTable({
+        data: datos,
+        columns: estructuraTabla,
+        rowId: 'id',
+        responsive: true,
+        "ordering": mostrar,
+        "searching": mostrar,
+        "lengthChange": mostrar,
+        "pageLength": 5,
+        "lengthMenu": [5, 10, 25, 50],
+        "language": {
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "_START_ al _END_ de _TOTAL_",
+            "sInfoEmpty": "0 al 0 de 0",
+            "sInfoFiltered": "(Total _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
+    });
+}
+
+//$("#dataTable").dataTable(
+//     {
+//        searching: false,
+//        language: {
+//            "decimal": "",
+//            "emptyTable": "No hay información",
+//            "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+//            "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+//            "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+//            "infoPostFix": "",
+//            "thousands": ",",
+//            "lengthMenu": "Mostrar _MENU_ Entradas",
+//            "loadingRecords": "Cargando...",
+//            "processing": "Procesando...",
+//            "search": "Buscar:",
+//            "zeroRecords": "Sin resultados encontrados",
+//            "paginate": {
+//                "first": "Primero",
+//                "last": "Ultimo",
+//                "next": "Siguiente",
+//                "previous": "Anterior"
+//            }
+//        },
+//   }
+//);
