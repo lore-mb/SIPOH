0ea34fa1643448d806fc5d383c478d87231f1645
Igmar Jovan Hernández <Igmhdmtz@gmail.com>
2020-09-11 15:58:08 -0500

    cambios test

--- PoderJudicial.SIPOH/PoderJudicial.SIPOH.UT/IgmaUT/TesteIgma.cs
+++ PoderJudicial.SIPOH/PoderJudicial.SIPOH.UT/IgmaUT/TesteIgma.cs
@@ -7,16 +7,32 @@ namespace PoderJudicial.SIPOH.UT.IgmaUT
     public class TesteIgma
     {
         [TestMethod]
-        public void TestMethod1()
+        ServerConnection cnx = null;
+
+        [TestInitialize]
+        public void GeneraConexion()
         {
-            var Numero = 12;
-            if (Numero < 18)
-            {
-                // Codigo aquí
-            } else { 
-                // Mas codigo
-            }
+            cnx = ServerConnection.GetConnection();
         }
+
+        [TestMethod]
+        public void CrearEjecucion()
+        {
+            EjecucionRepository repo = new EjecucionRepository(cnx);
+            Ejecucion ejecucion = new Ejecucion();
+            ejecucion.Solicitante = "SS";
+            ejecucion.DetalleSolicitante = "ESTE REGISTRO SE CREA DESDE LA UT DE WEB APP";
+            ejecucion.Solicitud = "0";
+            ejecucion.OtroSolicitante = "ESTA ES OTRA SOLICITUD";
+            ejecucion.NombreBeneficiario = "ROMAN";
+            ejecucion.ApellidoPBeneficiario = "ROMERO";
+            ejecucion.ApellidoMBeneficiario = "PARDO";
+            ejecucion.Interno = "S";
+            ejecucion.IdUsuario = 22;
+
+            int? idEjecucion = repo.CrearEjecucion(ejecucion, true, null);
+        }
+
 
     }
 }
