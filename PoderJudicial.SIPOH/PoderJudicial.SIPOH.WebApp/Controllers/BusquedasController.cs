fb0b8e7ebafd82c30b53ce340adc6f2e7325f25d
PBTO90 <bcss.90@outlook.com>
2020-10-08 09:28:16 -0500

    Se modifica titulo en iniciales

--- PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Controllers/BusquedasController.cs
+++ PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Controllers/BusquedasController.cs
@@ -13,22 +13,7 @@ namespace PoderJudicial.SIPOH.WebApp.Controllers
         // GET: Busquedas metodo para mandar a llamar con ajax
         public ActionResult BusquedaNumeroEjecucion()
         {
-            //Parametros al View Bag PickList
-            ViewBag.IdCircuito = Usuario.IdCircuito;
-            ViewBag.JuzgadosAcusatorios = new SelectList(new List<Ejecucion>());
-            ViewBag.DistritosPorCircuito = new SelectList(new List<Ejecucion>());
-            ViewBag.SalasAcusatorio = new SelectList(new List<Ejecucion>());
-            ViewBag.SalasTradicional = new SelectList(new List<Ejecucion>());
-            ViewBag.AnexosInicales = new SelectList(new List<Ejecucion>());
-            ViewBag.Solicitudes = new SelectList(new List<Ejecucion>());
-            ViewBag.Solicitantes = new SelectList(new List<Ejecucion>());
-
-            //Campos Banderas para validacio de "OTROS" de PickList
-            ViewBag.IdOtroAnexos = 10;
-            ViewBag.IdOtroSolicitud = 10;
-            ViewBag.IdOtroSolicitante = 10;
-            ViewBag.SalasAcusatorioTotal = 10;
-
+           
             return View();
         }
     }
