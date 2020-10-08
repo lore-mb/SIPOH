87751f7f0321f591b7781c4eb708c350ffab23ad
PBTO90 <bcss.90@outlook.com>
2020-10-08 09:02:41 -0500

    Se corrige nombre de vista, controlador de busquedas

--- PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Controllers/BusquedasController.cs
+++ PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Controllers/BusquedasController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PoderJudicial.SIPOH.Entidades;
+using PoderJudicial.SIPOH.WebApp.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +11,24 @@ namespace PoderJudicial.SIPOH.WebApp.Controllers
     public class BusquedasController : BaseController
     {
         // GET: Busquedas metodo para mandar a llamar con ajax
-        public ActionResult Busquedas()
+        public ActionResult BusquedaNumeroEjecucion()
         {
+            //Parametros al View Bag PickList
+            ViewBag.IdCircuito = Usuario.IdCircuito;
+            ViewBag.JuzgadosAcusatorios = new SelectList(new List<Ejecucion>());
+            ViewBag.DistritosPorCircuito = new SelectList(new List<Ejecucion>());
+            ViewBag.SalasAcusatorio = new SelectList(new List<Ejecucion>());
+            ViewBag.SalasTradicional = new SelectList(new List<Ejecucion>());
+            ViewBag.AnexosInicales = new SelectList(new List<Ejecucion>());
+            ViewBag.Solicitudes = new SelectList(new List<Ejecucion>());
+            ViewBag.Solicitantes = new SelectList(new List<Ejecucion>());
+
+            //Campos Banderas para validacio de "OTROS" de PickList
+            ViewBag.IdOtroAnexos = 10;
+            ViewBag.IdOtroSolicitud = 10;
+            ViewBag.IdOtroSolicitante = 10;
+            ViewBag.SalasAcusatorioTotal = 10;
+
             return View();
         }
     }
