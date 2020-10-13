a91a354a5d2968969fb1f8c8f7f13ce4dfcbce29
PBTO90 <bcss.90@outlook.com>
2020-10-13 09:17:28 -0500

    Se agrega validacion de Formularios para busqueda

---
+++ PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Models/EjecucionDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoderJudicial.SIPOH.WebApp.Models
+{
+    public class EjecucionDTO
+    {
+        public int IdEjecucion { set; get; }
+        public string NumeroEjecucion { set; get; }
+        public string NombreJuzgado { set; get; }
+        public string DetalleSolicitante { set; get; }
+        public string FechaEjecucion { set; get; }
+        public int IdSolicitud { set; get; }
+        public string DescripcionSolicitud { set; get; }
+        public string NombreBeneficiario { set; get; }
+        public string ApellidoPBeneficiario { set; get; }
+        public string ApellidoMBeneficiario { set; get; }
+        public string Tipo { set; get; }
+
+        //Partes relacionadas a la Causa de Ejecucion
+        public string NombreParte { get; set; }
+        public string ApellidoPParte { get; set; }
+        public string ApellidoMParte { get; set; }
+        public string TipoParte { get; set; }
+    }
+}
--- PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Scripts/Modules/Ejecucion/Busquedas/Busquedas.js
+++ PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Scripts/Modules/Ejecucion/Busquedas/Busquedas.js
@@ -1,5 +1,16 @@
 ﻿var EstatusRespuesta = { SIN_RESPUESTA: 0, OK: 1, ERROR: 2 }
 
+var estructuraTablaNumeroEejecucionPartes =
+    [{ data: 'noEjecucion', title: 'N° Ejecución', className: "text-center" },
+     { data: 'juzgadoEjecucion', title: 'Juzgado de Ejecución', className: "text-center" },
+     { data: 'fecha', title: 'Fecha Ejecución', className: "text-center" },
+     { data: 'beneficiario', title: 'Beneficiario', className: "text-center" },   
+     { data: 'tipoExpediente', title: 'Tipo Expediente', className: "text-center" },
+     { data: 'parteRelacionada', title: 'Parte Causas', className: "text-center" },
+     { data: 'parteRelacionada', title: 'Tipo Parte', className: "text-center" },
+     { data: 'causas', title: 'Detalle', className: "text-center" }];
+
+
 var estructuraTablaNumeroEjecucion = [{ data: 'noEjecucion', title: 'N° Ejecución', className: "text-center" }, { data: 'juzgadoEjecucion', title: 'Juzgado de Ejecución', className: "text-center" }, { data: 'fecha', title: 'Fecha Ejecución', className: "text-center" }, { data: 'beneficiario', title: 'Beneficiario', className: "text-center" }, { data: 'tipoExpediente', title: 'Tipo Expediente', className: "text-center" }, { data: 'causas', title: 'Detalle', className: "text-center" }];
 var numeroEjecucionDatos = [];
 var tablaNumeroEjecucion = null;
@@ -8,7 +19,211 @@ $(document).ready(function ()
 {
     //Pinta la tabla en el ejecucion
     tablaNumeroEjecucion = GeneraTablaDatos(tablaNumeroEjecucion, "dataTableNumeroEjecucion", numeroEjecucionDatos, estructuraTablaNumeroEjecucion, false, false, false);
+
+    //Funcionalidad de Elementos al Cargado
+    ElementosAlCargado();
 });
+
+function ElementosAlCargado()
+{
+    //Deshabilita option para juzgado acusatorio
+    $("#slctJuzgadoPorDistritos").prop('disabled', true);
+    $("#inpCausa").prop('disabled', true);
+
+    $('#slctDistrito').change(function ()
+    {
+        var idDistrito = $("#slctDistrito").find('option:selected').val();
+
+        //Metodo que contiene el proceso para llenado de pick List Juzgados Tradicionales
+        if (idDistrito != "" && idDistrito != null)
+        {
+            $("#slctJuzgadoPorDistritos").prop('disabled', false);
+            RecuperaJuzgadosAcusatorioTradicional(idDistrito);
+        }
+        else
+        {
+            $("#slctJuzgadoPorDistritos").prop('disabled', true);
+            $("#slctJuzgadoPorDistritos").html("");
+            $("#slctJuzgadoPorDistritos").append($("<option/>", { value: "", text: "SELECCIONAR OPCIÓN" }));
+            $("#inpCausa").prop('disabled', true);
+            $("#inpCausa").val("");
+        }
+    });
+
+    $('#slctJuzgadoPorDistritos').change(function ()
+    {
+        var idJuzgado = $("#slctJuzgadoPorDistritos").find('option:selected').val();
+
+        //Metodo que contiene el proceso para llenado de pick List Juzgados Tradicionales
+        if (idJuzgado != "" && idJuzgado != null)
+        {
+            $("#inpCausa").prop('disabled', false);    
+        }
+        else
+        {
+            $("#inpCausa").prop('disabled', true);
+            $("#inpCausa").val("");
+        }
+    });
+
+    $("#inpNuc").prop('disabled', true);
+
+    $('#slctJuzgadoAcusatorio').change(function ()
+    {
+        var idJuzgado = $("#slctJuzgadoAcusatorio").find('option:selected').val();
+
+        //Metodo que contiene el proceso para llenado de pick List Juzgados Tradicionales
+        if (idJuzgado != "" && idJuzgado != null)
+        {
+            $("#inpNuc").prop('disabled', false);
+        }
+        else
+        {
+            $("#inpNuc").prop('disabled', true);
+            $("#inpNuc").val("");
+        }
+    });
+
+
+    //Funcionalidad para validar formularios
+    var forms = document.getElementsByClassName('needs-validation');
+
+    Array.prototype.filter.call(forms, function (form)
+    {
+        form.addEventListener('submit', function (event)
+        {
+            var id = form.id;
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (id == "formPartesCausa" || id == "formBeneficiario")
+            {
+                //Valida solo campos especificos del formulario
+                var validateGroup = form.getElementsByClassName('validate-me');
+
+                for (var i = 0; i < validateGroup.length; i++)
+                {
+                    validateGroup[i].classList.add('was-validated');
+                }
+            }
+            else
+            {
+               //Valida todos los campos del formulario
+               form.classList.add('was-validated');
+            }
+
+            if (form.checkValidity() === true && id == "formPartesCausa")
+            {
+                BuscarEjecucionPorPartesBeneficiarios(true);
+            }
+
+            if (form.checkValidity() === true && id == "formBeneficiario")
+            {
+                BuscarEjecucionPorPartesBeneficiarios(false);
+            }
+
+            if (form.checkValidity() === true && id == "formCausasEjecucion")
+            {
+                alert("entro");
+            }
+
+            if (form.checkValidity() === true && id == "formNucEjecucion")
+            {
+                alert("entro");
+            }
+
+            if (form.checkValidity() === true && id == "formSolicitanteEjecucion")
+            {
+                alert("entro");
+            } 
+
+            if (form.checkValidity() === true && id == "formDetalleSolicitanteEjecucion")
+            {
+                alert("entro");
+            } 
+
+        }, false);
+    });
+}
+
+function RecuperaJuzgadosAcusatorioTradicional(idDistrito)
+{
+    if (idDistrito != null)
+    {
+        var parametros = { idDistrito: idDistrito }
+        SolicitudEstandarAjax("/Busquedas/ObtenerJuzgadosPorDistrito", parametros, GenerarOptionSelectJuzgadoPorDistrito);
+    }
+    else
+    {
+        alert("Error al obtener los datos");
+    }
+}
+
+function GenerarOptionSelectJuzgadoPorDistrito(data)
+{
+    if (data.Estatus == EstatusRespuesta.OK)
+    {
+        var numero = data.Data.length;
+
+        $("#slctJuzgadoPorDistritos").html("");
+
+        if (numero > 1)
+        {
+            $("#slctJuzgadoPorDistritos").append($("<option/>", { value: "", text: "SELECCIONAR OPCIÓN" }));
+        }
+
+        const ObjJuzgadoTra = [data.Data];
+        var $slcTradi = $('#slctJuzgadoPorDistritos');
+
+        $.each(ObjJuzgadoTra, function (id, juzgado)
+        {
+            for (var i = 0; i < juzgado.length; i++)
+            {
+                $slcTradi.append('<option value=' + juzgado[i].Value + '>' + juzgado[i].Text + '</option>');
+            }
+        });
+    }
+    else if (data.Estatus == EstatusRespuesta.ERROR)
+    {
+        customNotice(data.Mensaje, "Error:", "error", 3350);
+    }
+}
+
+function BuscarEjecucionPorPartesBeneficiarios(partes)
+{
+    var idNombre = partes ? "inpNombreParte" : "inpNombreBeneficiario";
+    var idApellidoPaterno = partes ? "inpApellidoPaternoParte" : "inpApellidoPaternoBeneficiario";
+    var idApellidoMaterno = partes ? "inpApellidoMaternoPartes" : "inpApellidoMaternoBeneficiario";
+
+    var nombre = $('#' + idNombre).val();
+    var apellidoPaterno = $('#' + idApellidoPaterno).val();
+    var apellidoMaterno = $('#' + idApellidoMaterno).val();
+
+    var parametros = { nombre: nombre, apellidoPaterno: apellidoPaterno, apellidoMaterno: apellidoMaterno };
+
+    if (partes)
+    {
+        SolicitudEstandarAjax("/Busquedas/BusquedaPartesCausa", parametros, ListarNumerosDeEjecucion);
+    }
+    else
+    {
+        SolicitudEstandarAjax("/Busquedas/BusquedaPorBeneficiario", parametros, ListarNumerosDeEjecucion);
+    } 
+}
+
+function ListarNumerosDeEjecucion(data)
+{
+    if (data.Estatus == EstatusRespuesta.OK)
+    {
+        var partesDeCausaEjecucion = data.Data.busquedaPartes;
+
+    }
+    else if (data.Estatus == EstatusRespuesta.ERROR)
+    {
+
+    }
+}
 
 function GeneraTablaDatos(tabla, idTablaHtml, datos, estructuraTabla, ordering, searching, lengthChange)
 {
@@ -58,4 +273,67 @@ function GeneraTablaDatos(tabla, idTablaHtml, datos, estructuraTabla, ordering, 
         }
     });
 }
+
+function SolicitudEstandarAjax(url, parametros, functionCallbackSuccess, functionCallbackError = null)
+{
+    $.ajax({
+        url: url,
+        type: "GET",
+        cache: false,
+        traditional: true,
+        contentType: "application/json; charset=utf-8",
+        data: parametros,
+        beforeSend: function ()
+        {
+            // $("#loading").fadeIn(); //Animacion Load
+        },
+        success: function (data)
+        {
+            functionCallbackSuccess(data);
+        },
+        error: function (jqXHR)
+        {
+            $("#loading").fadeOut();
+
+            var mensaje = '';
+
+            if (jqXHR.status === 0)
+            {
+                mensaje = 'No esta conectado, verifique su conexión.';
+            }
+            else if (jqXHR.status == 404)
+            {
+                mensaje = 'No se encontró la página solicitada, ERROR:404';
+            }
+            else if (jqXHR.status == 500)
+            {
+                mensaje = "Error interno del servidor, ERROR:500";
+            }
+            else if (exception === 'timeout')
+            {
+                mensaje = 'Error de Time Out.';
+            }
+            else if (exception === 'abort')
+            {
+                mensaje = 'Solicitud AJAX Abortada.';
+            }
+            else
+            {
+                mensaje = 'Error no detectado : ' + jqXHR.responseText;
+            }
+
+            if (functionCallbackError == null)
+            {
+                Alerta(mensaje, "large", "Error ");
+            }
+
+            if (functionCallbackError != null)
+            {
+                var data = mensaje;
+                functionCallbackError(data);
+            }
+        }
+    });
+}
+
 
