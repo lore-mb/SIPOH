1fa1ca49c891989f4e32ebaf518d6cbfe88a7c5e
Igmar Jovan <Igmhdmtz@gmail.com>
2020-11-25 12:41:46 -0600

    Agregado controlado de Control Iniciales

---
+++ PoderJudicial.SIPOH/PoderJudicial.SIPOH.WebApp/Controllers/InicialesControlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PoderJudicial.SIPOH.WebApp.Controllers
+{
+    public class InicialesControlController : Controller
+    {
+        // GET: InicialesControl
+        public ActionResult CrearInicialControl()
+        {
+            return View();
+        }
+    }
+}
